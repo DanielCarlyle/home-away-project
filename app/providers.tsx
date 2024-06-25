dbeccfed7aca1ec590a8e7b72510a479829e1588
Daniel <dan.carlyle@gmail.com>
2024-06-25 16:13:05 -0400

    add two

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,25 @@
-'use client';
-import { ThemeProvider } from './theme-provider';
+'use client'; 
 
-function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider
-      attribute='class'
-      defaultTheme='system'
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
-  );
-}
-export default Providers;
+import { ThemeProvider } from './theme-provider'; 
+import { Toaster } from '@/components/ui/toaster'; 
+ 
+function Providers({ children }: { children: React.ReactNode }) { 
+  return ( 
+    <> 
+      <Toaster /> 
+      <ThemeProvider 
+        attribute='class' 
+        defaultTheme='system' 
+        enableSystem 
+        disableTransitionOnChange 
+      > 
+        {children} 
+      </ThemeProvider> 
+    </> 
+  ); 
+} 
+
+ 
+export default Providers; 
+
+ 
